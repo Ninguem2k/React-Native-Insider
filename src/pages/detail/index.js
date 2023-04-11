95f4f030ef7052576d27baf45f8f843608447c2f
Ninguem2k <cassioriachinho@gmail.com>
2023-04-11 14:47:34 -0300

    Adicionando useLayoutEffect, e iseriando um header,

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,29 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import { useLayoutEffect } from "react";
+import { useRoute, useNavigation } from "@react-navigation/native";
+
+import { Entypo } from "@expo/vector-icons";
 
 export function Detail() {
+    const route = useRoute();
+    const navigation = useNavigation();
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            title: route.params?.data?route.params?.data.name : "Detalhe da receita",
+            
+            headerRight:()=>(
+            <Pressable onPress={()=>console.log("+++")}>
+                <Entypo name = "heart" size ={28} color="#FF4141"/>                
+            </Pressable>
+            )
+        })
+    },[navigation, route.params?.data])
+
     return (
         <View style={styles.container}>
             <Text>Página Detalhes</Text>
+            <Text>{route.params.data.name}</Text>
         </View>
     );
 }
